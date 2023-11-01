b6c43844d7e24844324ec10659a8cd8dc6d8a2a1
Sean Sovine <sean.r.sovine@gmail.com>
2023-10-31 22:03:12 -0400

    Start work on Set game solver

---
+++ set_solver/card.hpp
@@ -0,0 +1,74 @@
+#include <iostream>
+#include <vector>
+
+/* Putting definitions here for now, since small program. */
+
+enum class Color { red, green, blue };
+enum class Number { one, two, three };
+enum class Shading { filled, outlined, striped };
+enum class Shape { diamond, oval, squiggle };
+
+std::vector<Color> colors = {Color::red, Color::green, Color::blue};
+std::vector<Number> numbers = {Number::one, Number::two, Number::three};
+std::vector<Shading> shadings = {Shading::filled, Shading::outlined, Shading::striped};
+std::vector<Shape> shapes = {Shape::diamond, Shape::oval, Shape::squiggle};
+
+struct Card {
+  Color color;
+  Number number;
+  Shading shading;
+  Shape shape;
+
+  Card(Color c, Number n, Shading shad, Shape shap)
+      : color{c}, number{n}, shading{shad}, shape{shap} {}
+};
+
+std::ostream &operator<<(std::ostream &os, const Card &card) {
+  os << "[ ";
+  switch (card.color) {
+  case Color::red:
+    os << "R";
+    break;
+  case Color::green:
+    os << "G";
+    break;
+  case Color::blue:
+    os << "B";
+    break;
+  }
+  switch (card.number) {
+  case Number::one:
+    os << "1";
+    break;
+  case Number::two:
+    os << "2";
+    break;
+  case Number::three:
+    os << "3";
+    break;
+  }
+  switch (card.shading) {
+  case Shading::filled:
+    os << "F";
+    break;
+  case Shading::outlined:
+    os << "O";
+    break;
+  case Shading::striped:
+    os << "S";
+    break;
+  }
+  switch (card.shape) {
+  case Shape::diamond:
+    os << "D";
+    break;
+  case Shape::oval:
+    os << "O";
+    break;
+  case Shape::squiggle:
+    os << "S";
+    break;
+  }
+  os << " ]";
+  return os;
+}
---
+++ set_solver/find_sets.cpp
@@ -0,0 +1,66 @@
+#include <chrono>
+#include <iostream>
+#include <random>
+#include <set>
+#include <vector>
+
+// Defines Card struct and Color, Number, Shading, Shape enums,
+// and colors, numbers, shadings, and shapes vectors.
+#include "card.hpp"
+
+std::vector<Card> generateDeck() {
+  std::vector<Card> deck;
+  for (Color color : colors) {
+    for (Number number : numbers) {
+      for (Shading shading : shadings) {
+        for (Shape shape : shapes) {
+          deck.emplace_back(color, number, shading, shape);
+        }
+      }
+    }
+  }
+
+  return deck;
+}
+
+void shuffle(std::vector<Card> &deck) {
+  std::random_device rd;
+  std::mt19937 rng(rd());
+
+  for (int i = deck.size() - 1; i >= 0; i--) {
+    std::uniform_int_distribution<> distrib(0, i);
+    int j = distrib(rng);
+    Card temp = deck[i];
+    deck[i] = deck[j];
+    deck[j] = temp;
+  }
+}
+
+struct Candidate {
+  std::set<Card> cards{};
+};
+
+std::vector<Candidate> findSets(const std::vector<Card> &table) {
+  std::vector<Candidate> sets{};
+  // First generate candidates based on color, then
+  // check each for validity based on remaining traits.
+  return sets;
+}
+
+int main() {
+  std::vector<Card> deck = generateDeck();
+
+  shuffle(deck);
+  std::vector<Card> table{deck.begin(), deck.begin() + 12};
+
+  for (int i = 0; i < 3; i++) {
+    for (int j = 0; j < 4; j++) {
+      std::cout << table[j + 3 * i] << " ";
+    }
+    std::cout << std::endl;
+  }
+
+  std::cout << std::endl << "Generating sets:" << std::endl;
+}
+
+// g++ find_sets.cpp -o build/find_sets && echo "---" && ./build/find_sets
