582a00ad6c78715ad9c3beb81109dbe872c1b704
Sean Sovine <sean.r.sovine@gmail.com>
2023-11-03 09:25:20 -0400

    Move attribute vectors to a class

--- set_game_solver/find_sets.cpp
+++ set_game_solver/find_sets.cpp
@@ -6,15 +6,15 @@
 #include <vector>
 
 // Defines Card struct and Color, Number, Shading, Shape enums,
-// and colors, numbers, shadings, and shapes vectors.
+// and Attributes class w/ vectors for iteration.
 #include "card.hpp"
 
-std::vector<Card> generateDeck() {
+std::vector<Card> generateDeck(Attributes attrs) {
   std::vector<Card> deck;
-  for (Color color : colors) {
-    for (Number number : numbers) {
-      for (Shading shading : shadings) {
-        for (Shape shape : shapes) {
+  for (Color color : attrs.colors) {
+    for (Number number : attrs.numbers) {
+      for (Shading shading : attrs.shadings) {
+        for (Shape shape : attrs.shapes) {
           deck.emplace_back(color, number, shading, shape);
         }
       }
@@ -72,7 +72,7 @@ std::vector<Candidate> getColorDistinctCands(const std::array<std::vector<Card>,
   return cands;
 }
 
-std::vector<Candidate> findSets(const std::vector<Card> &table) {
+std::vector<Candidate> findSets(const std::vector<Card> &table, Attributes attrs) {
   auto sameOrDiff = [](const std::array<Card, 3> &cand) -> bool {
     if (!(cand[0].number == cand[1].number && cand[0].number == cand[2].number) &&
         !(cand[0].number != cand[1].number && cand[1].number != cand[2].number &&
@@ -98,7 +98,7 @@ std::vector<Candidate> findSets(const std::vector<Card> &table) {
 
   // Generate color-homogeneous candidates.
   int i = 0;
-  for (Color color : colors) {
+  for (Color color : attrs.colors) {
     // Collect cards with this color.
     std::vector<Card> colorCards{};
     for (Card card : table) {
@@ -133,7 +133,8 @@ std::vector<Candidate> findSets(const std::vector<Card> &table) {
 }
 
 int main() {
-  std::vector<Card> deck = generateDeck();
+  Attributes attrs{};
+  std::vector<Card> deck = generateDeck(attrs);
 
   shuffle(deck);
   std::vector<Card> table{deck.begin(), deck.begin() + 12};
@@ -147,7 +148,7 @@ int main() {
   }
 
   std::cout << std::endl << "Generating sets:" << std::endl;
-  auto sets = findSets(table);
+  auto sets = findSets(table, attrs);
 
   // Print sets found, if any.
   if (sets.size() == 0) {
