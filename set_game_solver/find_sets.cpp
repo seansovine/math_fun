fdf7c88cec51fca281b9f41bb4735e0c8c642046
Sean Sovine <sean.r.sovine@gmail.com>
2024-03-01 21:08:50 -0500

    Buffer output to make more flexible (could write to file, e.g.)

--- set_game_solver/find_sets.cpp
+++ set_game_solver/find_sets.cpp
@@ -1,34 +1,43 @@
+#include <sstream>
+
 #include "game.hpp"
 
 /* Output. */
 
-void printTable(const Cards &table) {
-  std::cout << "Cards on table:" << std::endl << std::endl;
+std::string outputForSetup(const Cards &table) {
+  std::ostringstream buffer{};
+  buffer << "Cards on table:" << std::endl << std::endl;
   for (int i = 0; i < 3; i++) {
     for (int j = 0; j < 4; j++) {
-      std::cout << table[j + 3 * i] << " ";
+      buffer << table[j + 3 * i] << " ";
     }
-    std::cout << std::endl;
+    buffer << std::endl;
   }
+
+  return buffer.str();
 }
 
-void printResults(const Results &results) {
-  std::cout << " -- Generated " << results.redCandidates << " red candidates." << std::endl
-            << "              " << results.greenCandidates << " green candidates." << std::endl
-            << "              " << results.blueCandidates << " blue candidates." << std::endl
-            << "              " << results.distinctCandidates << " color-distinct candidates." << std::endl;
+std::string outputForResults(const Results &results) {
+  std::ostringstream buffer{};
+  buffer << " -- Generated " << results.redCandidates << " red candidates." << std::endl
+         << "              " << results.greenCandidates << " green candidates." << std::endl
+         << "              " << results.blueCandidates << " blue candidates." << std::endl
+         << "              " << results.distinctCandidates << " color-distinct candidates." << std::endl;
 
   if (results.sets.size() == 0) {
-    std::cout << std::endl << "No sets found!" << std::endl;
+    buffer << std::endl << "No sets found!" << std::endl;
   } else {
-    std::cout << std::endl << "" << results.sets.size() << " sets found! They are:" << std::endl << std::endl;
+    buffer << std::endl << "" << results.sets.size() << " sets found! They are:" << std::endl << std::endl;
   }
+
   for (Candidate cand : results.sets) {
     for (Card card : cand.cards) {
-      std::cout << card << " ";
+      buffer << card << " ";
     }
-    std::cout << std::endl;
+    buffer << std::endl;
   }
+
+  return buffer.str();
 }
 
 /* Main. */
@@ -36,14 +45,15 @@ void printResults(const Results &results) {
 int main() {
   std::cout << std::endl << "Generating and shuffling deck." << std::endl;
   Cards deck = generateDeck();
-  shuffle(deck);
 
+  shuffle(deck);
   Cards table{deck.begin(), deck.begin() + 12};
-  printTable(table);
+  deck.erase(deck.begin(), deck.begin() + 12);
+  std::cout << outputForSetup(table);
 
   std::cout << std::endl << "Finding sets:" << std::endl << std::endl;
   Results sets = findSets(table);
-  printResults(sets);
+  std::cout << outputForResults(sets);
 }
 
 // c && g++ find_sets.cpp -o build/find_sets && echo "---" && ./build/find_sets && echo
