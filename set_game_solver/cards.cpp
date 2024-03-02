c49df7abd5d210cf122b3c142b9866bd2aeb8e71
Sean Sovine <sean.r.sovine@gmail.com>
2024-03-02 13:10:42 -0500

    Add colored output on Linux

--- set_game_solver/cards.cpp
+++ set_game_solver/cards.cpp
@@ -1,16 +1,26 @@
 #include "cards.hpp"
 
+#if __linux__
+  #define R_SYM "\e[1;31mR\e[0m"
+  #define G_SYM "\e[1;32mG\e[0m"
+  #define B_SYM "\e[1;34mB\e[0m"
+#else
+  #define R_SYM "R"
+  #define G_SYM "G"
+  #define B_SYM "B"
+#endif
+
 std::ostream &operator<<(std::ostream &os, const Card &card) {
   os << "[ ";
   switch (card.color) {
   case Color::red:
-    os << "R";
+    os << R_SYM;
     break;
   case Color::green:
-    os << "G";
+    os << G_SYM;
     break;
   case Color::blue:
-    os << "B";
+    os << B_SYM;
     break;
   }
   switch (card.number) {
