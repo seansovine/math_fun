6ecbeff751c638bc8d8d5fc1fd3760f2bb1bf1c9
Sean Sovine <sean.r.sovine@gmail.com>
2024-03-12 21:18:58 -0400

    Small polishing

--- set_game_solver/find_sets.cpp
+++ set_game_solver/find_sets.cpp
@@ -2,19 +2,20 @@
 
 /* Run set game solver. */
 
+static constexpr size_t CARDS_ON_TABLE = 15;
+
 int main() {
-  static const size_t CARDS_ON_TABLE = 15;
-  static_assert(CARDS_ON_TABLE <= 3 * 3 * 3 * 3);
-
   std::cout << std::endl << "Generating and shuffling deck." << std::endl << std::endl;
-  auto deckBuilder = ShuffledDeckBuilder();
+  Cards table = ShuffledDeckBuilder{}.dealCards(CARDS_ON_TABLE);
 
-  Cards table = deckBuilder.dealCards(CARDS_ON_TABLE);
   std::cout << outputTable(table);
 
   std::cout << std::endl << "Finding sets:" << std::endl << std::endl;
   Results results = SetFinder(std::move(table)).find();
+
   std::cout << outputForResults(results);
 }
+
+static_assert(CARDS_ON_TABLE <= 3 * 3 * 3 * 3);
 
 // Linux build and run: clear && ./set -r
